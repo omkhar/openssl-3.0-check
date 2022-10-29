ad8ee4ff3cd0604acda26c2da2d485fe73a648b9
Omkhar Arasaratnam <omkhar@gmail.com>
2022-10-29 13:50:50 -0400

    Update openssl-check.sh
    Cleaned up code, added some error checking

--- openssl-check.sh
+++ openssl-check.sh
@@ -2,10 +2,31 @@
 # License GPL
 # Copyright 2022 Omkhar Arasaratnam 
 
-for FILE in `find / -type f -executable -print`
+if [ ${UID} -ne 0 ]
+then
+	echo "Not running as root, we may not be able to read all binaries on the system [enter to continue]"
+	read
+fi
+
+LDD=`which ldd`
+EXIT=$?
+if [ ${EXIT} -ne 0 ]
+then
+	echo "Couldn't find ldd, exiting"
+	exit 1
+fi
+
+FIND=`which find`
+EXIT=$?
+if [ ${EXIT} -ne 0 ]
+then
+	echo "Couldn't find find, exiting"
+	exit 1
+fi
+
+for FILE in `${FIND} / -type f -executable -print`
 do
-  SSL=""
-  SSL=`ldd ${FILE} 2> /dev/null | grep -i libssl.so.3`
+  ${LDD} ${FILE} 2> /dev/null | grep -i "libssl.so.3" &> /dev/null
   EXIT=$?
   if [ ${EXIT} -eq 0 ]
   then
