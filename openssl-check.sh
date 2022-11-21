53fbb83cb35a9900161c8c0f921a088dca106e59
Rob Dailey <rob@wargam.es>
2022-11-21 17:32:16 -0500

    os specific tweaks

--- openssl-check.sh
+++ openssl-check.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # License GPL
 # Copyright 2022 Omkhar Arasaratnam
-
-#set -x 
+#   sexified by Rob Dailey
 
 function cleanup_kids { 
   for job in $(jobs -lp); do
@@ -35,7 +34,7 @@ if [[ "${MAX_RUNNING}" == 0 ]]; then
 
   if [[ "${MAX_RUNNING}" == 0 ]]; then
     MAX_RUNNING=$(sysctl hw.ncpu 2>/dev/null | tr -cd '[:digit:]')
-    MAX_RUNNING=$(( ${MAX_RUNNING:-"4"} / 8 ))
+    MAX_RUNNING=$(( ${MAX_RUNNING:-"4"} / 4 ))
     if [[ "${MAX_RUNNING}" -lt "4" ]]; then
       MAX_RUNNING=4  # seems to be optimal min
     fi
@@ -49,36 +48,42 @@ fi
 
 echo "Running with: [${MAX_RUNNING}]:Jobs [${DEPTH}]:Depth"
 
-if [ ${UID} -ne 0 ]
-then
-        echo "Not running as root, we may not be able to read all binaries on the system [enter to continue]"
-        read
+if [[ ${UID} -ne 0 ]]; then
+  echo "Not running as root, we may not be able to read all binaries on the system [enter to continue]"
+  read
 fi
 
-if [[ "${os}" == 'linux' || "${os}" == *"bsd"* ]]; then
+if [[ "${os}" == 'linux' ]];then 
   LDD=$(which ldd)
+  WHICH_LDD_EXIT=$?
   LIB="libssl.so.3"
+elif [[ "${os}" == *"bsd"* ]]; then
+  LDD=$(which ldd)
+  WHICH_LDD_EXIT=$?
+  LIB="libssl.so.3"
+  LOCALS_ONLY="-fstype local"
 elif [[ "${os}" == 'darwin' ]]; then
   LDD=$(which otool)
+  WHICH_LDD_EXIT=$?
   LDD_FLAGS="-L"
   LIB="libssl.3.dylib"
+  LOCALS_ONLY="-fstype local"
 else
   echo "unsupported OS [${os}]"
   exit 1
 fi
 
-if [[ -z "${LDD}" ]]; then
+if [[ "${WHICH_LDD_EXIT}" -ne 0 ]]; then
   echo "Couldn't find ldd, exiting"
   exit 1
 fi
 
 
-FIND=`which find`
-EXIT=$?
-if [ ${EXIT} -ne 0 ]
-then
-        echo "Couldn't find find, exiting"
-        exit 1
+FIND=$(which find)
+WHICH_FIND_EXIT=$?
+if [[ ${WHICH_FIND_EXIT} -ne 0 ]]; then
+  echo "Couldn't find find, exiting"
+  exit 1
 fi
 
 
@@ -129,7 +134,7 @@ while read -r dir_path; do
     wait -n
   done
 
-done < <(find / -fstype local -maxdepth ${DEPTH} \( -path "/proc" -o -path "/System/Volumes/Data" \) -prune -o -type d -print 2>/dev/null | sort -r)
+done < <(find / -maxdepth ${DEPTH} \( -path "/proc" -o -path "/System/Volumes" \) -prune -o ${MAC_LOCALS_ONLY} -type d -print 2>/dev/null | sort -r)
 
 # catch any leftover files
 while read -r dir_path; do
